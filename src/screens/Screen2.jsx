31857ef3e720affc808349ffe8411bd2258c66e1
Pabs125 <pablon.ocampol@autonoma.edu.co>
2024-05-21 16:54:40 -0500

    Feature: Create the new screens and routing file

---
+++ src/screens/Screen2.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Text } from 'react-native-elements'
+export const Screen2 = () => {
+  return (
+    <Text>Screen2</Text>
+  )
+}
---
+++ src/screens/Screen3.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Text } from 'react-native-elements'
+export const Screen3 = () => {
+  return (
+    <Text>Screen3</Text>
+  )
+}
